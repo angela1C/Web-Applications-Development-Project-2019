d22ce47e5c9efa20f21ed2693c0a774dbfafc527
angela1C <fange21@icloud.com>
2020-05-20 01:43:01 +0100

    dom styling and templates updated

---
+++ A/jscript.js
@@ -0,0 +1,17 @@
+
+
+
+function changeFontColour() {
+    document.getElementById("myh2").style.color ="red";
+ 
+ }
+ 
+function changeFontSize() {
+    document.getElementById("myh2").style.fontSize ="smaller";
+ 
+ }
+ 
+function changeFontStyle() {
+    document.getElementById("myh2").style.fontStyle = "italic";
+ 
+ }
